184859f68b3774353cbc7ce4e274ced8645d38d3
Michel Migdal <mm@mgdl.fr>
2021-08-26 13:12:03 +0200

    Register Arrays

--- sycl/include/sycl/detail/array.hpp
+++ sycl/include/sycl/detail/array.hpp
@@ -18,6 +18,88 @@ template <int dimensions> class id;
 template <int dimensions> class range;
 namespace detail {
 
+template <typename T, int dimensions> class register_array;
+
+// template<typename T, int dimensions>
+// class register_array : public std::array<T, dimensions> {
+//     static_assert(dimensions > 3);
+// };
+
+template <typename T> class register_array<T, 1> {
+private:
+  T v0_{};
+
+public:
+  register_array() = default;
+
+  explicit register_array(T v0) : v0_(v0) {}
+
+  inline T &operator[](size_t idx) {
+    (void)idx;
+    return v0_;
+  }
+
+  inline T operator[](size_t idx) const {
+    (void)idx;
+    return v0_;
+  }
+
+  inline bool operator==(const register_array &rhs) const {
+    return v0_ == rhs.v0_;
+  }
+
+  inline bool operator!=(const register_array &rhs) const {
+    return v0_ != rhs.v0_;
+  }
+};
+
+template <typename T> class register_array<T, 2> {
+private:
+  T v0_{}, v1_{};
+
+public:
+  register_array() = default;
+
+  explicit register_array(T v0, T v1) : v0_(v0), v1_(v1) {}
+
+  inline T &operator[](size_t idx) { return idx == 0 ? v0_ : v1_; }
+
+  inline T operator[](size_t idx) const { return idx == 0 ? v0_ : v1_; }
+
+  inline bool operator==(const register_array &rhs) const {
+    return v0_ == rhs.v0_ && v1_ == rhs.v1_;
+  }
+
+  inline bool operator!=(const register_array &rhs) const {
+    return v0_ != rhs.v0_ || v1_ == rhs.v1_;
+  }
+};
+
+template <typename T> class register_array<T, 3> {
+private:
+  T v0_{}, v1_{}, v2_{};
+
+public:
+  register_array() = default;
+
+  explicit register_array(T v0, T v1, T v2) : v0_(v0), v1_(v1), v2_(v2) {}
+
+  inline T &operator[](size_t idx) {
+    return idx == 0 ? v0_ : (idx == 1 ? v1_ : v2_);
+  }
+
+  inline T operator[](size_t idx) const {
+    return idx == 0 ? v0_ : (idx == 1 ? v1_ : v2_);
+  }
+
+  inline bool operator==(const register_array &rhs) const {
+    return v0_ == rhs.v0_ && v1_ == rhs.v1_ && v2_ == rhs.v2_;
+  }
+
+  inline bool operator!=(const register_array &rhs) const {
+    return v0_ != rhs.v0_ || v1_ == rhs.v1_ || v2_ != rhs.v2_;
+  }
+};
 template <int dimensions = 1> class array {
   static_assert(dimensions >= 1, "Array cannot be 0-dimensional.");
 
@@ -87,27 +169,17 @@ public:
   // Returns true iff all elements in 'this' are equal to
   // the corresponding elements in 'rhs'.
   bool operator==(const array<dimensions> &rhs) const {
-    for (int i = 0; i < dimensions; ++i) {
-      if (this->common_array[i] != rhs.common_array[i]) {
-        return false;
-      }
-    }
-    return true;
+    return this->common_array == rhs.common_array;
   }
 
   // Returns true iff there is at least one element in 'this'
   // which is not equal to the corresponding element in 'rhs'.
   bool operator!=(const array<dimensions> &rhs) const {
-    for (int i = 0; i < dimensions; ++i) {
-      if (this->common_array[i] != rhs.common_array[i]) {
-        return true;
-      }
-    }
-    return false;
+    return this->common_array != rhs.common_array;
   }
 
 protected:
-  size_t common_array[dimensions];
+  register_array<size_t, dimensions> common_array{};
   __SYCL_ALWAYS_INLINE void check_dimension(int dimension) const {
 #ifndef __SYCL_DEVICE_ONLY__
     if (dimension >= dimensions || dimension < 0) {
