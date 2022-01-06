c5b1adeac018e6631017ccf98c20207414ccbd2e
pgorlani <paolo.gorlani@codeplay.com>
2022-01-06 09:59:40 +0000

    apply clang format

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -780,9 +780,10 @@ void CodeGenModule::Release() {
                                   llvm::DenormalMode::IEEE);
     getModule().addModuleFlag(llvm::Module::Override, "nvvm-reflect-prec-sqrt",
                               getTarget().getTargetOpts().NVVMCudaPrecSqrt);
-    getModule().addModuleFlag(llvm::Module::Override, "nvvm-reflect-approx-tanhf",
+    getModule().addModuleFlag(llvm::Module::Override,
+                              "nvvm-reflect-approx-tanhf",
                               getTarget().getTargetOpts().NVVMCudaApproxTanhf);
-   }
+  }
 
   if (LangOpts.EHAsynch)
     getModule().addModuleFlag(llvm::Module::Warning, "eh-asynch", 1);
