5e66852d484c37c04184231f5b38429662345c8f
pgorlani <paolo.gorlani@codeplay.com>
2022-08-02 09:32:10 +0100

    remove unecessary diagnostics

--- clang/lib/Sema/SemaSYCL.cpp
+++ clang/lib/Sema/SemaSYCL.cpp
@@ -4653,9 +4653,9 @@ void Sema::finalizeSYCLDelayedAnalysis(const FunctionDecl *Caller,
   if (!Callee->isDefined() && !Callee->getBuiltinID() &&
       !Callee->isReplaceableGlobalAllocationFunction() &&
       !isSYCLUndefinedAllowed(Callee, getSourceManager())) {
-//    Diag(Loc, diag::err_sycl_restrict) << Sema::KernelCallUndefinedFunction;
-//    Diag(Callee->getLocation(), diag::note_previous_decl) << Callee;
-//    Diag(Caller->getLocation(), diag::note_called_by) << Caller;
+    Diag(Loc, diag::err_sycl_restrict) << Sema::KernelCallUndefinedFunction;
+    Diag(Callee->getLocation(), diag::note_previous_decl) << Callee;
+    Diag(Caller->getLocation(), diag::note_called_by) << Caller;
   }
 }
 
