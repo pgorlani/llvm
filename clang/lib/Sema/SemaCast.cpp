cc200576874d0632349e0173920142d3d584805b
Victor Lomuller <victor@codeplay.com>
2022-10-13 11:52:29 +0000

    Apply Victor's format

--- clang/lib/Sema/SemaCast.cpp
+++ clang/lib/Sema/SemaCast.cpp
@@ -2562,7 +2562,6 @@ static TryCastResult TryAddressSpaceCast(Sema &Self, ExprResult &SrcExpr,
                                          QualType DestType, bool CStyle,
                                          unsigned &msg, CastKind &Kind,
                                          SourceRange OpRange) {
-
   if (!Self.getLangOpts().OpenCL && !Self.getLangOpts().SYCLIsDevice)
     // FIXME: As compiler doesn't have any information about overlapping addr
     // spaces at the moment we have to be permissive here.
