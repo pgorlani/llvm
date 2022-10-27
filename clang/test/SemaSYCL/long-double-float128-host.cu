b2ef9462baaaf55fc7c45ed513ecc241f610d1c9
pgorlani <paolo.gorlani@codeplay.com>
2022-10-27 03:26:49 -0700

    add test on long double __float128 support for host

---
+++ clang/test/SemaSYCL/long-double-float128-host.cu
@@ -0,0 +1,14 @@
+// RUN: %clang_cc1 -triple x86_64-linux-gnu -fsycl-is-host -internal-isystem %S/Inputs -sycl-std=2020 -fsyntax-only %s
+// RUN: %clang_cc1 -triple x86_64-linux-gnu -fsycl-is-device -internal-isystem %S/Inputs -sycl-std=2020 -fsyntax-only %s
+// RUN: %clang_cc1 -triple x86_64-linux-gnu -fsycl-is-host -fcuda-is-device -internal-isystem %S/Inputs -sycl-std=2020 -fsyntax-only %s
+
+// Test long double and __float128 support for host. 
+
+__float128 foo_0(__float128 P) { return P; }
+long double foo_1(long double P) { return P;}
+
+int main() {
+  foo_0(1.0);
+  foo_1(1.0);
+  return 0;
+}
