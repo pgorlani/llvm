b37bfe83841008cc7c8aa51b349ffe8377009a55
pgorlani <paolo.gorlani@codeplay.com>
2022-11-02 07:11:16 -0700

    add check on CUDA builtin attribute

---
+++ clang/test/SemaSYCL/builtins.cu
@@ -0,0 +1,15 @@
+// RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -aux-triple x86_64-unknown-linux-gnu \
+// RUN:     -fsycl-is-device -internal-isystem %S/Inputs -sycl-std=2020 \
+// RUN:     -fsyntax-only -verify %s
+
+// Check that aux builtins have the correct __device__ attribute
+
+__attribute__((device)) void df() {
+  int x = __nvvm_read_ptx_sreg_ctaid_x();
+}
+
+void fun() {
+  df();
+}
+
+// expected-no-diagnostics
