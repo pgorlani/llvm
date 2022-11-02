7353c1084529c16ad1a459fbc432cc8d99e02f36
pgorlani <paolo.gorlani@codeplay.com>
2022-11-02 08:15:07 -0700

    change builtin test for using ast

--- clang/test/SemaSYCL/builtins.cu
+++ clang/test/SemaSYCL/builtins.cu
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -aux-triple x86_64-unknown-linux-gnu \
 // RUN:     -fsycl-is-device -internal-isystem %S/Inputs -sycl-std=2020 \
-// RUN:     -fsyntax-only -verify %s
+// RUN:     -ast-dump %s | FileCheck %s
 
 // Check that aux builtins have the correct __device__ attribute
 
@@ -12,4 +12,8 @@ void fun() {
   df();
 }
 
-// expected-no-diagnostics
+// CHECK: FunctionDecl {{.*}} __nvvm_read_ptx_sreg_ctaid_x
+// CHECK-NEXT: BuiltinAttr 
+// CHECK-NEXT: NoThrowAttr
+// CHECK-NEXT: ConstAttr
+// CHECK-NEXT: CUDADeviceAttr
